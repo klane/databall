06b18b63d783259b956c01ffb164fb299fa2ad65
Kevin Lane <lane.kevin.a@gmail.com>
2017-03-05 21:57:21 -0800

    Add database builder

---
+++ nba/database_builder.py
@@ -0,0 +1,47 @@
+from nba_py.league import GameLog, TeamStats
+import sqlite3
+import time
+
+
+def add_team_games(conn, season, if_exists='append'):
+    table = GameLog(season=season_str(season), player_or_team='T').overall()
+    table.drop(labels_to_drop(table.columns, ['ABBREV', 'NAME', 'SEASON', 'VIDEO']), axis=1, inplace=True)
+    table['SEASON'] = season
+    table.to_sql('team_games', conn, if_exists=if_exists, index=False)
+
+
+def add_team_stats(conn, season, if_exists='append'):
+    table = TeamStats(season=season_str(season)).overall()
+    table.drop(labels_to_drop(table.columns, ['CF', 'NAME', 'RANK']), axis=1, inplace=True)
+    table['SEASON'] = season
+    table.to_sql('team_stats', conn, if_exists=if_exists, index=False)
+
+
+def build_database(start_season, end_season):
+    conn = sqlite3.connect('nba.db')
+
+    for season in range(start_season, end_season+1):
+        print 'Reading ' + season_str(season)
+
+        if season == start_season:
+            add_team_stats(conn, season, 'replace')
+            time.sleep(1)
+
+            add_team_games(conn, season, 'replace')
+            time.sleep(1)
+        else:
+            add_team_stats(conn, season)
+            time.sleep(1)
+
+            add_team_games(conn, season)
+            time.sleep(1)
+
+    conn.close()
+
+
+def labels_to_drop(column_names, list_of_strings):
+    return [col for col in column_names if any([x for x in list_of_strings if x in col])]
+
+
+def season_str(season):
+    return str(season) + '-' + str(season+1)[-2:]
