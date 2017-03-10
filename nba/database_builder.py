7de05e03205154c6ebb492fe7d647805b010f152
Kevin Lane <lane.kevin.a@gmail.com>
2017-03-09 23:39:44 -0800

    Loop over seasons in database builder functions

--- nba/database_builder.py
+++ nba/database_builder.py
@@ -4,21 +4,37 @@ import sqlite3
 import time
 
 
-def add_player_game_stats(conn, season, if_exists='append'):
-    table = GameLog(season=season_str(season), player_or_team='P').overall()
-    table.drop(labels_to_drop(table.columns, ['ABBREV', 'NAME', 'SEASON', 'VIDEO']), axis=1, inplace=True)
-    table['SEASON'] = season
-    table.to_sql('player_game_stats', conn, if_exists=if_exists, index=False)
+def add_player_game_stats(conn, start_season, end_season, if_exists='append', sleep=1):
+    for season in range(start_season, end_season + 1):
+        print 'Reading ' + season_str(season) + ' player game stats'
+        table = GameLog(season=season_str(season), player_or_team='P').overall()
+        table.drop(labels_to_drop(table.columns, ['ABBREV', 'NAME', 'SEASON', 'VIDEO']), axis=1, inplace=True)
+        table['SEASON'] = season
+
+        if season == start_season:
+            table.to_sql('player_game_stats', conn, if_exists=if_exists, index=False)
+        else:
+            table.to_sql('player_game_stats', conn, if_exists='append', index=False)
+
+        time.sleep(sleep)
 
 
-def add_player_season_stats(conn, season, if_exists='append'):
-    table = PlayerStats(season=season_str(season)).overall()
-    table.drop(labels_to_drop(table.columns, ['ABBREV', 'CF', 'NAME', 'RANK']), axis=1, inplace=True)
-    table['SEASON'] = season
-    table.to_sql('player_season_stats', conn, if_exists=if_exists, index=False)
+def add_player_season_stats(conn, start_season, end_season, if_exists='append', sleep=1):
+    for season in range(start_season, end_season + 1):
+        print 'Reading ' + season_str(season) + ' player season stats'
+        table = PlayerStats(season=season_str(season)).overall()
+        table.drop(labels_to_drop(table.columns, ['ABBREV', 'CF', 'NAME', 'RANK']), axis=1, inplace=True)
+        table['SEASON'] = season
+
+        if season == start_season:
+            table.to_sql('player_season_stats', conn, if_exists=if_exists, index=False)
+        else:
+            table.to_sql('player_season_stats', conn, if_exists='append', index=False)
+
+        time.sleep(sleep)
 
 
-def add_teams(conn):
+def add_teams(conn, sleep=1):
     teams = TeamList().info()[0:30]
     teams['CITY'] = 'TEMP'
     teams['NICKNAME'] = 'TEMP'
@@ -26,51 +42,49 @@ def add_teams(conn):
 
     for ID in teams.TEAM_ID:
         teams.loc[teams.TEAM_ID == ID, columns] = TeamDetails(ID).background()[columns].values
-        time.sleep(0.5)
+        time.sleep(sleep)
 
     teams.drop(labels_to_drop(teams.columns, ['LEAGUE_ID', 'YEAR']), axis=1, inplace=True)
     teams.to_sql('teams', conn, if_exists='replace')
 
 
-def add_team_game_stats(conn, season, if_exists='append'):
-    table = GameLog(season=season_str(season), player_or_team='T').overall()
-    table.drop(labels_to_drop(table.columns, ['ABBREV', 'NAME', 'SEASON', 'VIDEO']), axis=1, inplace=True)
-    table['SEASON'] = season
-    table.to_sql('team_game_stats', conn, if_exists=if_exists, index=False)
-
-
-def add_team_season_stats(conn, season, if_exists='append'):
-    table = TeamStats(season=season_str(season)).overall()
-    table.drop(labels_to_drop(table.columns, ['CF', 'NAME', 'RANK']), axis=1, inplace=True)
-    table['SEASON'] = season
-    table.to_sql('team_season_stats', conn, if_exists=if_exists, index=False)
-
-
-def build_database(start_season, end_season):
-    conn = sqlite3.connect('nba.db')
-
-    for season in range(start_season, end_season+1):
-        print 'Reading ' + season_str(season)
+def add_team_game_stats(conn, start_season, end_season, if_exists='append', sleep=1):
+    for season in range(start_season, end_season + 1):
+        print 'Reading ' + season_str(season) + ' team game stats'
+        table = GameLog(season=season_str(season), player_or_team='T').overall()
+        table.drop(labels_to_drop(table.columns, ['ABBREV', 'NAME', 'SEASON', 'VIDEO']), axis=1, inplace=True)
+        table['SEASON'] = season
 
         if season == start_season:
-            add_player_game_stats(conn, season, 'replace')
-            time.sleep(1)
-            add_player_season_stats(conn, season, 'replace')
-            time.sleep(1)
-            add_team_game_stats(conn, season, 'replace')
-            time.sleep(1)
-            add_team_season_stats(conn, season, 'replace')
-            time.sleep(1)
+            table.to_sql('team_game_stats', conn, if_exists=if_exists, index=False)
         else:
-            add_player_game_stats(conn, season)
-            time.sleep(1)
-            add_player_season_stats(conn, season)
-            time.sleep(1)
-            add_team_game_stats(conn, season)
-            time.sleep(1)
-            add_team_season_stats(conn, season)
-            time.sleep(1)
+            table.to_sql('team_game_stats', conn, if_exists='append', index=False)
 
+        time.sleep(sleep)
+
+
+def add_team_season_stats(conn, start_season, end_season, if_exists='append', sleep=1):
+    for season in range(start_season, end_season + 1):
+        print 'Reading ' + season_str(season) + ' team season stats'
+        table = TeamStats(season=season_str(season)).overall()
+        table.drop(labels_to_drop(table.columns, ['CF', 'NAME', 'RANK']), axis=1, inplace=True)
+        table['SEASON'] = season
+
+        if season == start_season:
+            table.to_sql('team_season_stats', conn, if_exists=if_exists, index=False)
+        else:
+            table.to_sql('team_season_stats', conn, if_exists='append', index=False)
+
+        time.sleep(sleep)
+
+
+def build_database(start_season, end_season, if_exists='replace', sleep=1):
+    conn = sqlite3.connect('nba.db')
+    add_teams(conn, sleep)
+    add_player_game_stats(conn, start_season, end_season, if_exists, sleep)
+    add_player_season_stats(conn, start_season, end_season, if_exists, sleep)
+    add_team_game_stats(conn, start_season, end_season, if_exists, sleep)
+    add_team_season_stats(conn, start_season, end_season, if_exists, sleep)
     conn.close()
 
 
