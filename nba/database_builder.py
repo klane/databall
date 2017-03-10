d599ffc9708dec0dbe4b49c0bab20da0abd56891
Kevin Lane <lane.kevin.a@gmail.com>
2017-03-09 23:13:24 -0800

    Change table names

--- nba/database_builder.py
+++ nba/database_builder.py
@@ -4,18 +4,18 @@ import sqlite3
 import time
 
 
-def add_player_games(conn, season, if_exists='append'):
+def add_player_game_stats(conn, season, if_exists='append'):
     table = GameLog(season=season_str(season), player_or_team='P').overall()
     table.drop(labels_to_drop(table.columns, ['ABBREV', 'NAME', 'SEASON', 'VIDEO']), axis=1, inplace=True)
     table['SEASON'] = season
-    table.to_sql('player_games', conn, if_exists=if_exists, index=False)
+    table.to_sql('player_game_stats', conn, if_exists=if_exists, index=False)
 
 
-def add_player_stats(conn, season, if_exists='append'):
+def add_player_season_stats(conn, season, if_exists='append'):
     table = PlayerStats(season=season_str(season)).overall()
     table.drop(labels_to_drop(table.columns, ['ABBREV', 'CF', 'NAME', 'RANK']), axis=1, inplace=True)
     table['SEASON'] = season
-    table.to_sql('player_stats', conn, if_exists=if_exists, index=False)
+    table.to_sql('player_season_stats', conn, if_exists=if_exists, index=False)
 
 
 def add_teams(conn):
@@ -32,18 +32,18 @@ def add_teams(conn):
     teams.to_sql('teams', conn, if_exists='replace')
 
 
-def add_team_games(conn, season, if_exists='append'):
+def add_team_game_stats(conn, season, if_exists='append'):
     table = GameLog(season=season_str(season), player_or_team='T').overall()
     table.drop(labels_to_drop(table.columns, ['ABBREV', 'NAME', 'SEASON', 'VIDEO']), axis=1, inplace=True)
     table['SEASON'] = season
-    table.to_sql('team_games', conn, if_exists=if_exists, index=False)
+    table.to_sql('team_game_stats', conn, if_exists=if_exists, index=False)
 
 
-def add_team_stats(conn, season, if_exists='append'):
+def add_team_season_stats(conn, season, if_exists='append'):
     table = TeamStats(season=season_str(season)).overall()
     table.drop(labels_to_drop(table.columns, ['CF', 'NAME', 'RANK']), axis=1, inplace=True)
     table['SEASON'] = season
-    table.to_sql('team_stats', conn, if_exists=if_exists, index=False)
+    table.to_sql('team_season_stats', conn, if_exists=if_exists, index=False)
 
 
 def build_database(start_season, end_season):
@@ -53,22 +53,22 @@ def build_database(start_season, end_season):
         print 'Reading ' + season_str(season)
 
         if season == start_season:
-            add_player_games(conn, season, 'replace')
+            add_player_game_stats(conn, season, 'replace')
             time.sleep(1)
-            add_player_stats(conn, season, 'replace')
+            add_player_season_stats(conn, season, 'replace')
             time.sleep(1)
-            add_team_games(conn, season, 'replace')
+            add_team_game_stats(conn, season, 'replace')
             time.sleep(1)
-            add_team_stats(conn, season, 'replace')
+            add_team_season_stats(conn, season, 'replace')
             time.sleep(1)
         else:
-            add_player_games(conn, season)
+            add_player_game_stats(conn, season)
             time.sleep(1)
-            add_player_stats(conn, season)
+            add_player_season_stats(conn, season)
             time.sleep(1)
-            add_team_games(conn, season)
+            add_team_game_stats(conn, season)
             time.sleep(1)
-            add_team_stats(conn, season)
+            add_team_season_stats(conn, season)
             time.sleep(1)
 
     conn.close()
