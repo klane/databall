e4da2eaf3d0224a8d0daa47c57ff8e88aa1598d9
Kevin Lane <lane.kevin.a@gmail.com>
2017-07-29 10:58:47 -0700

    Register Scrapy pipeline and add download delay

--- covers/settings.py
+++ covers/settings.py
@@ -2,3 +2,8 @@ BOT_NAME = 'covers'
 
 SPIDER_MODULES = ['covers.spiders']
 NEWSPIDER_MODULE = 'covers.spiders'
+ITEM_PIPELINES = {
+    'covers.pipelines.GamePipeline': 1,
+}
+
+DOWNLOAD_DELAY = 1.0
