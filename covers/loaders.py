5ccb24739a23a520db1ce12b781e7c546dcdbef3
Kevin Lane <lane.kevin.a@gmail.com>
2017-08-12 09:25:54 -0700

    Remove reference to unicode module for Python 3

--- covers/loaders.py
+++ covers/loaders.py
@@ -5,14 +5,14 @@ from scrapy.loader.processors import MapCompose, TakeFirst
 
 
 class GameLoader(ItemLoader):
-    default_input_processor = MapCompose(unicode.strip)
+    default_input_processor = MapCompose(str.strip)
     default_output_processor = TakeFirst()
 
-    location_in = MapCompose(unicode.strip, lambda x: x if len(x) > 0 else u'vs')
-    result_in = MapCompose(unicode.strip, unicode.split)
+    location_in = MapCompose(str.strip, lambda x: x if len(x) > 0 else u'vs')
+    result_in = MapCompose(str.strip, str.split)
     score_in = MapCompose(partial(re.findall, u'\d+-\d+'), lambda x: x.split(u'-'), int)
     opponent_score_in = MapCompose(partial(re.findall, u'\d+-\d+'), lambda x: x.split(u'-')[1], int)
-    spread_result_in = MapCompose(unicode.strip, unicode.split, lambda x: x if x != u'-' else None)
+    spread_result_in = MapCompose(str.strip, str.split, lambda x: x if x != u'-' else None)
     spread_in = MapCompose(lambda x: x.strip().split()[1], lambda x: 0 if x == u'PK' else x,
                            lambda x: float(x) if x != u'-' else None)
     over_under_result_in = spread_result_in
