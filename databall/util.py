79ce140f98ed2a1d217617770a0c2ce3cc00300f
Kevin Lane <lane.kevin.a@gmail.com>
2017-09-11 11:34:07 -0700

    Add select columns function

--- databall/util.py
+++ databall/util.py
@@ -3,3 +3,7 @@ from IPython.display import display, HTML
 
 def print_df(df):
     display(HTML(df.to_html(index=False)))
+
+
+def select_columns(df, attributes):
+    return df[:, [index for index, column in enumerate(df.columns) if any(name in column for name in attributes)]]
