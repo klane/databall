e03102755f32e3556e90f0751d995a35436b490b
Wheaties466 <Mwheat42289@gmail.com>
2020-02-18 20:54:07 -0500

    Update database_builder.py

--- databall/database_builder.py
+++ databall/database_builder.py
@@ -4,7 +4,7 @@ import pandas as pd
 import sqlite3
 import time
 from nba_api.stats.endpoints import leaguegamefinder
-from nba_api.stats.static import teams
+from nba_api.stats.static import teams as TEAMS
 from nba_api.stats.endpoints import leaguedashplayerstats
 from nba_api.stats.endpoints import teamgamelogs
 from nba_api.stats.endpoints import leaguedashteamstats
@@ -68,7 +68,7 @@ def add_teams(conn, sleep=1):
     conn.execute('DROP TABLE IF EXISTS teams')
     conn.execute('VACUUM')
     conn.execute('CREATE TABLE teams (ID INTEGER, ABBREVIATION TEXT, CITY TEXT, MASCOT TEXT)')
-    teams = teams.get_teams()[0:30].get_data_frames()[0]
+    teams = TEAMS.get_teams()[0:30].get_data_frames()[0]
     teams.drop(labels_to_drop(teams.columns, ['LEAGUE_ID', 'YEAR']), axis=1, inplace=True)
     teams.rename(columns={'TEAM_ID': 'ID'}, inplace=True)
     teams['CITY'] = 'TEMP'
