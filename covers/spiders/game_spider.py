cbd3ae9af510b70d1932b1ce67f9b9e46450858e
Kevin Lane <lane.kevin.a@gmail.com>
2022-03-26 09:21:38 -0700

    Fix scraper to parse previous seasons

--- covers/spiders/game_spider.py
+++ covers/spiders/game_spider.py
@@ -1,13 +1,15 @@
 import re
-from collections import deque
 
 import pandas as pd
 from scrapy import Request, Spider
+from scrapy.selector import SelectorList
 
 from covers.items import Game
 from covers.loaders import GameLoader
 
-base_url = 'https://www.covers.com'
+
+def get_text(response):
+    return response.xpath('text()').get().strip()
 
 
 class GameSpider(Spider):
@@ -30,14 +32,14 @@ class GameSpider(Spider):
             teams = teams.split(',')
 
         self.start_urls = [
-            f'{base_url}/sport/basketball/nba/teams/main/{team}/{season}'
+            f'https://www.covers.com/sport/basketball/nba/teams/main/{team}/{season}'
             for team in teams
         ]
 
     def parse(self, response):
-        table_class = 'table covers-CoversMatchups-Table covers-CoversResults-Table'
+        past_results = '//div[@id="TP_pastResults"]'
 
-        for row in response.xpath(f'//table[@class="{table_class}"]/tbody/tr'):
+        for row in response.xpath(f'{past_results}//table/tbody/tr'):
             loader = GameLoader(item=Game(), selector=row)
             loader.add_xpath('date', 'td[1]/text()')
             loader.add_xpath('home', 'td[2]/a/text()')
@@ -51,38 +53,30 @@ class GameSpider(Spider):
             loader.add_xpath('spread', 'td[4]/text()')
             loader.add_xpath('over_under_result', 'td[5]/span/text()')
             loader.add_xpath('over_under', 'td[5]/text()')
-            loader.add_value('response_url', response._url)
+            loader.add_value('response_url', response.url)
 
             # add missing fields
             item = loader.load_item()
             fields = ['spread_result', 'spread', 'over_under_result', 'over_under']
-            fields = list(filter(lambda f: f not in item, fields))
+            fields = [f for f in fields if f not in item]
 
             for f in fields:
                 item[f] = None
 
             yield item
 
-        # get list of previous seasons for the current team
-        history = deque(response.xpath('//option'))
-        season = _get_next_season(history)
-
         # find selected season
-        while season and season.xpath('@selected').extract_first() is None:
-            season = _get_next_season(history)
+        season = response.xpath(f'{past_results}//span[@id="TP-Season-Select"]')
+        season = get_text(season)
 
-        # find next season to scrape, some pages have duplicate seasons in the drop down
-        while season and season.xpath('@value').extract_first() in response.url:
-            season = _get_next_season(history)
+        # get other seasons for the current team
+        history = response.xpath(f'{past_results}//div[@id="TP-Season-Drop"]/li/a')
+
+        # get seasons prior to the selected season
+        history = SelectorList(row for row in history if get_text(row) < season)
 
         # scrape previous season if one exists
-        if season:
-            url = base_url + season.xpath('@value').extract_first()
+        url = history.xpath('@href').get()
+
+        if url is not None:
             yield Request(response.urljoin(url), callback=self.parse)
-
-
-def _get_next_season(history):
-    try:
-        return history.popleft()
-    except IndexError:
-        return None
