2c0bc0396f8f48ab6bfa91f3c5e1007a9067f6f5
Kevin Lane <lane.kevin.a@gmail.com>
2022-05-02 08:41:45 -0700

    Check that scraped scores and results match database

--- databall/covers/pipelines.py
+++ databall/covers/pipelines.py
@@ -1,6 +1,6 @@
 from sqlalchemy import select
 
-from databall.db import Covers, Games, Teams
+from databall.db import Covers, Games, Teams, TeamStats
 from databall.db.session import Session
 
 
@@ -26,19 +26,34 @@ class GamePipeline:
     def store_item(self, item, spider):
         # find game by opponent and date or raise exception if not found
         query = (
-            select(Games.id)
+            select(Games)
             .join(Teams, Teams.id == Games.away_team_id)
             .where(
                 (Teams.abbreviation == item['opponent'])
                 & (Games.game_date == item['date'])
             )
         )
-        game_id = self.session.execute(query).scalars().one()
+        game = self.session.execute(query).scalars().one()
+
+        # check that scraped score matches database
+        for team, score in zip(['home', 'away'], ['score', 'opponent_score']):
+            query = select(TeamStats.pts).where(
+                (TeamStats.game_id == game.id)
+                & (TeamStats.team_id == getattr(game, f'{team}_team_id'))
+            )
+            pts = self.session.execute(query).scalars().one()
+
+            if item[score] != pts:
+                raise ValueError(f'Scraped {team} team score does not match database')
+
+        # check that scraped result matches database
+        if item['result'] != game.home_wl.name:
+            raise ValueError('Scraped result does not match database')
 
         # insert row into database if not present
-        if game_id not in Covers.primary_keys.values:
+        if game.id not in Covers.primary_keys.values:
             row = Covers(
-                game_id=game_id,
+                game_id=game.id,
                 home_spread=item['spread'],
                 home_spread_result=item['spread_result'],
                 over_under=item['over_under'],
